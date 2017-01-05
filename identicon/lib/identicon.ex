f3f5be966dca013d210acd4a9b90f48decc3e1d2
Kayleigh Rix <kayleigh.rix@outplay.com>
2017-01-05 16:29:19 +0000

    [add][identicon] build_grid

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,6 +3,25 @@ defmodule Identicon do
     input
     |> hash_input
     |> pick_colour
+    |> build_grid
+  end
+
+  def build_grid(image) do
+    %Identicon.Image{hex: hex} = image
+
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+    row ++ [second, first]
   end
 
   def pick_colour(image) do
