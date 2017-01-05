3160fbe4fa6d3821656d4fbdd26678c1a0b93b37
Kayleigh Rix <kayleigh.rix@outplay.com>
2017-01-05 22:40:18 +0000

    [add][discuss] create changeset and list topics

--- discuss/web/controllers/topic_controller.ex
+++ discuss/web/controllers/topic_controller.ex
@@ -2,9 +2,23 @@ defmodule Discuss.TopicController do
   use Discuss.Web, :controller
   alias Discuss.Topic
 
-  def new(conn, params) do
-    changeset = Topic.changeset(%Topic{})
+  def index(conn, _params) do
+    topics = Repo.all(Topic)
+    render conn, "index.html", topics: topics
+  end
 
+  def new(conn, _params) do
+    changeset = Topic.changeset(%Topic{})
     render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"topic" => topic}) do
+    changeset = Topic.changeset(%Topic{}, topic)
+
+    case Repo.insert(changeset) do
+      {:ok, post} -> IO.inspect(post)
+      {:error, changeset} ->
+        render conn, "new.html", changeset: changeset
+    end
   end
 end
