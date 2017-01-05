25f8c3324f0df6e9123d5dd1121eb919931aea76
Kayleigh Rix <kayleigh.rix@outplay.com>
2017-01-05 15:51:31 +0000

    [add][identicon] pick_colour

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,6 +2,11 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_input
+    |> pick_colour
+  end
+
+  def pick_colour(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
+    %Identicon.Image{image | colour: {r, g, b}}
   end
 
   def hash_input(input) do
