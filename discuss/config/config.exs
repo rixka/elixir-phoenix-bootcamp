76b632718e7a9944d5c233a60bfe62cfa9da8121
Kayleigh Rix <kayleigh.rix@outplay.com>
2017-01-06 11:18:13 +0000

    [fix][discuss] config

--- discuss/config/config.exs
+++ discuss/config/config.exs
@@ -31,6 +31,6 @@ config :ueberauth, Ueberauth,
     github: { Ueberauth.Strategy.Github, [] }
   ]
 
-config :ueberauth, Ueberauth.Strategy.Github.Oauth,
+config :ueberauth, Ueberauth.Strategy.Github.OAuth,
   client_id: "XXXXX",
   client_secret: "XXX"
